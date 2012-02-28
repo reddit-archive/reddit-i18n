46c3c495b83bcffcb3d09b7ab2d7da17a6d17553
Keith Mitchell <kemitche@reddit.com>
2012-02-27 16:13:46 -0800

    Prepare for use of transifex.net as translation editor

--- setup.py
+++ setup.py
@@ -9,19 +9,12 @@ except ImportError:
 setup(
     name='reddit_i18n',
     version="",
-    #description='',
-    #author='',
-    #author_email='',
-    #url='',
-    install_requires=["Pylons==0.9.6.2"],
+    install_requires=["Pylons==0.9.6.2",
+                      "transifex-client"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'reddit_i18n': ['i18n/*/LC_MESSAGES/*.mo']},
-    #message_extractors = {'reddit_i18n': [
-    #        ('**.py', 'python', None),
-    #        ('templates/**.mako', 'mako', None),
-    #        ('public/**', 'ignore', None)]},
     entry_points="""
     [paste.app_factory]
     main = reddit_i18n.config.middleware:make_app
